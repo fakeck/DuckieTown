629fe892051ce9a67f652cfac62a80b3418ac156
jiranyi <92947427+jiranyi@users.noreply.github.com>
2024-12-09 12:42:54 +0100

    Added lane controller tuning

--- lane_following_controller/bash_scripts/bringup_dt_core.sh
+++ lane_following_controller/bash_scripts/bringup_dt_core.sh
@@ -7,5 +7,6 @@ docker run \
  -it --rm -e VEHICLE_NAME=$(echo $VEHICLE_NAME) --name=dt-core --privileged --memory "800m" --memory-swap="2800m" \
  -v /data:/data -v /var/run/avahi-daemon/socket:/var/run/avahi-daemon/socket \
  -v /home/duckie/vnc-docker/user_code_mount_dir/project/lane_following_controller/config/default.yaml:/code/catkin_ws/src/dt-core/packages/line_detector/config/line_detector_node/default.yaml \
+ -v /home/duckie/vnc-docker/user_code_mount_dir/project/lane_following_controller/config/default_control.yaml:/code/catkin_ws/src/dt-core/packages/lane_control/config/lane_controller_node/default.yaml \
  duckietown/dt-core:daffy-arm64v8 \
  roslaunch duckietown_demos lane_following.launch
